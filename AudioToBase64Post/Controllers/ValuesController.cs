ff057ef10713eed1023e84ef376914837dc70d81
anitheesh <nitheesh.arumugam@claysys.info>
2024-01-31 16:22:25 +0530

    Add project files.

---
+++ AudioToBase64Post/Controllers/ValuesController.cs
@@ -0,0 +1,40 @@
+﻿// ValuesController
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.IO;
+using System.Net;
+using System.Threading.Tasks;
+using AudioToBase64Post.Model;
+
+namespace AudioToBase64Post.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ValuesController : ControllerBase
+    {
+        // POST api/<ValuesController>
+        [HttpPost]
+        public async Task<IActionResult> Post([FromBody] Class model)
+        {
+
+            try
+            {
+                
+
+                    // Construct the response message including EmployeeID and Email
+                    var responseMessage = new
+                    {
+                        model.File,
+                        model.EmployeeID,
+                        model.Email
+                    };
+                    return Ok(responseMessage);
+                
+            }
+            catch (Exception e)
+            {
+                return StatusCode((int)HttpStatusCode.InternalServerError, new { error = e.Message });
+            }
+        }
+    }
+}
---
+++ AudioToBase64Post/Model/Class.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.Xml.Linq;
+
+namespace AudioToBase64Post.Model
+{
+    public class Class
+    {
+      
+        public string EmployeeID { get; set; }
+        
+        public string Email { get; set; }
+       
+        public string File { get; set; }
+    }
+}
